b5e6117ca58ef03bd60d7603a7f8be43da57bd77
Lucas Granjeon <lg1027520@wcupa.edu>
2024-09-24 11:24:02 -0400

    Completed Week 4 assignments

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -4,6 +4,28 @@ public class Problem2 {
         // TODO: Implement the insert method
         // This method should insert a new node with the given value at the specified position
         // If the position is out of bounds, insert the node at the end of the list
-        return null;
+
+        if (position < 0) {
+            position = 0;
+        }
+        if (position == 0) {
+            ListNode newNode = new ListNode(val);
+            newNode.next = head;
+            return newNode;
+        }
+        ListNode current = head;
+        for (int i = 0; i < position - 1; i++) {
+            if (current == null) {
+                break;
+            }
+            current = current.next;
+        }
+        if (current != null) {
+            ListNode newNode = new ListNode(val);
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+        
+        return head;
     }
 }
