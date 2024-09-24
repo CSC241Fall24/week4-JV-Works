a63de222d3c8523adeabfbba4382c71f69cd194d
Lucas Granjeon <lg1027520@wcupa.edu>
2024-09-24 12:25:12 -0400

    Commit

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,13 +1,6 @@
 public class Problem2 {
     public static ListNode insert(ListNode head, int val, int position) {
-        if (position < 0) {
-            position = 0;
-        }
-        if (position == 0) {
-            ListNode newNode = new ListNode(val);
-            newNode.next = head;
-            return newNode;
-        }
+        
         ListNode current = head;
         for (int i = 0; i < position - 1; i++) {
             if (current == null) {
