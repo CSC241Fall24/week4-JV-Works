39bfb2b3b082d02cb2fc8898b25019951e261e2e
Lucas Granjeon <lg1027520@wcupa.edu>
2024-09-24 12:55:10 -0400

    Commit

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -11,17 +11,10 @@ public class Problem2 {
         ListNode current = head;
         int currentPosition = 0;
         
-        // Traverse the list to find the correct position
-        while (current.next != null && currentPosition < position - 1) {
+        // Traverse the list to find the correct position for insertion
+        while (currentPosition < position - 1 && current.next != null) {
             current = current.next;
             currentPosition++;
-        }
-
-        // Handle insertion by checking whether position 1 insertion needs special handling
-        if (position == 1) {
-            ListNode temp = head;
-            newNode.next = temp;
-            return newNode;
         }
         
         // Insert the new node at the correct position
