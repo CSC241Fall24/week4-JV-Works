2031dfe72646d473b53eb3ef8844cabd31ba2f9d
Lucas Granjeon <lg1027520@wcupa.edu>
2024-09-24 13:30:18 -0400

    Commit

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -2,31 +2,25 @@ public class Problem2 {
     public static ListNode insert(ListNode head, int val, int position) {
         ListNode newNode = new ListNode(val);
         
-        // Special case: if the list is empty or we're inserting at the head (position 0)
-        if (position == 0 || head == null) {
+        // Special case: if the list is empty or we're inserting at the beginning (position 1 or less)
+        if (head == null || position <= 1) {
             newNode.next = head;
             return newNode;
         }
         
         ListNode current = head;
-        int currentPosition = 0;
+        int currentPosition = 1;
         
-        // Traverse the list to find the correct position
+        // Traverse the list to find the correct position or the last node
         while (current.next != null && currentPosition < position - 1) {
             current = current.next;
             currentPosition++;
         }
-
-        // Handle insertion by checking whether position 1 insertion needs special handling
-        if (position == 1) {
-            ListNode temp = head;
-            newNode.next = temp;
-            return newNode;
+        
+        // Insert the new node
+        newNode.next = current.next;
+        current.next = newNode;
+        
+        return head;
     }
-    // Insert the new node at the correct position
-    newNode.next = current.next;
-    current.next = newNode;
-    
-    return head;
-}
 }
