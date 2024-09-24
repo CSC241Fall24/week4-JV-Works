95d2bb31f64b57e9ee03c12a59c3c1e031b4a719
Lucas Granjeon <lg1027520@wcupa.edu>
2024-09-24 12:44:13 -0400

    Commit

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -2,31 +2,25 @@ public class Problem2 {
     public static ListNode insert(ListNode head, int val, int position) {
         ListNode newNode = new ListNode(val);
         
-        // If inserting at the head (position 0)
-        if (position == 0) {
+        // If inserting at the head (position 0) or the list is empty
+        if (position == 0 || head == null) {
             newNode.next = head;
             return newNode;
         }
         
         ListNode current = head;
-        for (int i = 0; i < position - 1; i++) {
-            if (current == null) {
-                break;
-            }
+        int currentPosition = 0;
+        
+        // Traverse the list until we reach the desired position or the end of the list
+        while (current.next != null && currentPosition < position - 1) {
             current = current.next;
+            currentPosition++;
         }
         
-        if (current != null) {
-            newNode.next = current.next;
-            current = newNode;
-        } else {
-            // If current is null, it means position is out of bounds, so we append at the end
-            current = head;
-            while (current.next != null) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
+        // Insert the new node
+        newNode.next = current.next;
+        current.next = newNode;
+        
         return head;
     }
 }
