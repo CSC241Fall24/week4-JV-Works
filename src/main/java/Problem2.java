8b99e9d8bb9cb48c7df260a68bd67138637e3457
Lucas Granjeon <lg1027520@wcupa.edu>
2024-09-24 12:37:45 -0400

    Commit

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -3,24 +3,26 @@ public class Problem2 {
         ListNode newNode = new ListNode(val);
         
         // If inserting at the head (position 0)
-        if (position == 1) {
-            newNode = head;
+        if (position == 0) {
+            newNode.next = head;
             return newNode;
         }
         
         ListNode current = head;
-        for (int i = 0; i < position - 1; i++) {
+        ListNode prev = null;
+        for (int i = 0; i < position; i++) {
             if (current == null) {
                 break;
             }
+            prev = current;
             current = current.next;
         }
         
-        if (current != null) {
-            newNode.next = current.next;
-            current.next = newNode;
+        if (prev != null) {
+            prev.next = newNode;
+            newNode.next = current;
         } else {
-            // If current is null, it means position is out of bounds, so we append at the end
+            // If prev is null, it means position is out of bounds, so we append at the end
             current = head;
             while (current.next != null) {
                 current = current.next;
